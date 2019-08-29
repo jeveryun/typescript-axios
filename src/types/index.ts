4aa172e50ab068a322d648e363d32044d1210dba
jever <jeveryun@qq.com>
2019-08-29 15:07:29 +0800

    扩展接口

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,29 @@
+export interface Axios {
+  request(config: AxiosRequestConfig): AxiosPromise
+
+  get(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  delete(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  head(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  options(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+
+  put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+
+  patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+}
+
+export interface AxiosInstance extends Axios {
+  (config: AxiosRequestConfig): AxiosPromise
+
+  (url: string, config?: AxiosRequestConfig): AxiosPromise
+}
+
 export interface AxiosRequestConfig {
-  url: string
+  url?: string
   method?: Method
   data?: any
   params?: any
@@ -33,10 +57,7 @@ export interface AxiosResponse {
   request: any
 }
 
-export interface AxiosPromise extends Promise<AxiosResponse> {
-
-}
-
+export interface AxiosPromise extends Promise<AxiosResponse> {}
 
 export interface AxiosError extends Error {
   config: AxiosRequestConfig
